8ddfda0595bca2566f227edf360bbc7f1707daff
MackazElo <mackaz2000@gmail.com>
2023-07-17 23:55:47 +0200

    Working prototype with table and fonts

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ app.use(bodyParser.urlencoded({
 app.use("/", express.static("public"));
 
   app.post('/submit', (req, res) => {
-    const content = req.body.content;
-    const styles = req.body.styles;
-    const file_name = req.body.file_name;
-    // Do something with the content and styles (e.g. generate an image)
-    const puppeteer = require('puppeteer');
+          const content = req.body.content;
+          const styles = req.body.styles;
+          const file_name = req.body.file_name;
+          // Do something with the content and styles (e.g. generate an image)
+          const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    defaultViewport: {width: 500, height: 500}
-    
-});
-  toprint = `<style> body{ position: absolute;
-    left: -8px;
-    top: -8px;
-    overflow: hidden;} #recived { ${styles} }</style> <div id='recived'>${content}</div>`
-  const page = await browser.newPage();
-  await page.setContent(toprint);
-  await page.screenshot({ path: `${file_name}.png`});
-  await browser.close();
-  res.send("Zapisano" + toprint)
-})();
+      (async () => {
+        const browser = await puppeteer.launch({
+          defaultViewport: {width: 500, height: 500}
+          
+      });
+        toprint = `<style> body{ position: absolute;
+          left: -8px;
+          top: -8px;
+          overflow: hidden;} #recived { ${styles} }</style> <div id='recived'>${content}</div>`
+        const page = await browser.newPage();
+        await page.setContent(toprint);
+        await page.screenshot({ path: `${file_name}.png`});
+        await browser.close();
+        res.send("Zapisano" + toprint)
+      })();
 
   });
   
